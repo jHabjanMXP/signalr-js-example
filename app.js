584c498395d287302c5aa102d571da280eaaaffd
Jernej Habjan <jhabjan@marinexchange.com>
2022-02-25 12:29:53 +0100

    Added auth, groups, support, object as response and request

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/chatHub").build();
+// get token from PEM login
+var loginToken = "eyJhbGciOiJIUzI1NiI......";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/DataChangeDispatcherMpos", { accessTokenFactory: () => loginToken }).build();
+
+// we could join multiple groups
+var groupName = "group123";
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessage", function (outgoingMessage) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${outgoingMessage.user} says ${outgoingMessage.message}`;
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+    connection.invoke("AddToGroup", {groupName}).then(()=>{
+        document.getElementById("sendButton").disabled = false;
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+
 }).catch(function (err) {
     return console.error(err.toString());
 });
@@ -21,8 +33,31 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    connection.invoke("SendMessage", {user, message}).catch(function (err) {
         return console.error(err.toString());
     });
+    event.preventDefault();
+});
+
+document.getElementById("sendButtonGroup").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessageGroup", {user, message ,groupName}).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    event.preventDefault();
+});
+
+
+document.getElementById("sendButtonNotOurGroup").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessageGroupNotOur", {user, message}).catch(function (err) {
+        return console.error(err.toString());
+    });
+
     event.preventDefault();
 });
